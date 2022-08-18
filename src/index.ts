4bb3226d368237f3b3b1846f7a1a84b8d7e4e302
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2022-08-18 15:14:49 +0300

    feat: expose organization store, only the information and not the setters

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import useOrganization from './store/useOrganization';
+
 export * from './request';
 export * from './routing';
 export * from './authentication';
+
+export default {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useOrganization: useOrganization(({ organizations, selectedOrganization }) => ({
+    organizations,
+    selectedOrganization,
+  })),
+};
