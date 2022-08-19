3bf4c8ec864dafb4433a3d3b01fa2b92071d4d2a
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2022-08-19 11:47:39 +0300

    chore: add organization declaration to export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import useOrganization from './store/useOrganization';
+import useOrganization, { Organization } from './store/useOrganization';
 
 export * from './request';
 export * from './routing';
 export * from './authentication';
+
+export type { Organization };
 
 export default {
   // eslint-disable-next-line react-hooks/rules-of-hooks
