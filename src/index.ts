dd2fd5e4c0d54401cd093b348432e76c612ac8bc
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2022-10-31 10:47:35 +0200

    fix: remove sentry for now

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
-import * as Sentry from '@sentry/browser';
-import { BrowserTracing } from '@sentry/tracing';
-
 import { default as useOrganizationModule, Organization } from './store/useOrganization';
-
-Sentry.init({
-  dsn: 'https://0b4fc3c60dde4486a5a41c6c40450dce@o470508.ingest.sentry.io/4504055714807808',
-  integrations: [new BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 /*
  * Eliminate any other information from the useOrganization zustand state coming out
