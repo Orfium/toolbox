baa0ac4b9e1737bf396ddaada70ddf6ade6b4705
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2023-11-09 17:57:40 +0200

    fix: add missing exports

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,4 +1,8 @@
-export { type AuthenticationContextValue } from './authentication';
-export { type OrfiumProductsContextValue } from './orfium-products';
+export {
+  type AuthenticationContextValue,
+  type DecodedTokenResponse,
+  type User,
+} from './authentication';
+export { type OrfiumProductsContextValue, type Product } from './orfium-products';
 export { type OrganizationsContextValue } from './organizations';
 export { type TopBarUtilitySectionContextValue } from './top-bar-utility-section';
