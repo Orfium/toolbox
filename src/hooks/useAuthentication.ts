4e5d11e9b90dbae1992e4b0b4bd6fba3582eae1d
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2023-11-11 01:50:15 +0200

    feat: update exports, fix a bug with authentication and switchorg and update docs

--- src/hooks/useAuthentication.ts
+++ src/hooks/useAuthentication.ts
@@ -2,3 +2,5 @@ import { useContext } from 'react';
 import { AuthenticationContext } from '../contexts/authentication';
 
 export const useAuthentication = () => useContext(AuthenticationContext);
+
+export type UseAuthenticationReturnValue = ReturnType<typeof useAuthentication>;
--- src/providers/Organizations.tsx
+++ src/providers/Organizations.tsx
@@ -1,7 +1,18 @@
+import { AuthorizationParams } from '@auth0/auth0-spa-js';
 import { ReactNode, useCallback, useMemo } from 'react';
 import { OrganizationsContext } from '../contexts/organizations';
-import useOrganization from '../store/organizations';
+import useOrganization, { Organization } from '../store/organizations';
 import { getAuth0Client } from '../utils/auth';
+
+export type SwitchOrganization = (orgID: Organization['org_id']) => void;
+
+export type _SwitchOrganization = (
+  orgID: Organization['org_id'],
+  options?: {
+    withLogout?: boolean;
+    authorizationParams?: AuthorizationParams;
+  }
+) => void;
 
 export function Organizations(props: { children: ReactNode }) {
   const { children } = props;
@@ -19,7 +30,7 @@ export function Organizations(props: { children: ReactNode }) {
     return [];
   }, [organizationsDict, organizationsList]);
 
-  const switchOrganization = useCallback(
+  const switchOrganization: SwitchOrganization = useCallback(
     async function (orgID) {
       const client = await getAuth0Client();
       await client.logout({ openUrl: false });
@@ -33,9 +44,35 @@ export function Organizations(props: { children: ReactNode }) {
     [setSelectedOrganization]
   );
 
+  // This is a more configurable switchOrganization for internal use only.
+  // It can be configured to not cause a logout.
+  // It can also accept a custom AuthorizationParams config object, to use in loginWithRedirect()
+  const _switchOrganization: _SwitchOrganization = useCallback(
+    async function (orgID, options = { withLogout: false }) {
+      const client = await getAuth0Client();
+
+      if (options.withLogout) {
+        await client.logout({ openUrl: false });
+      }
+      await client.loginWithRedirect({
+        authorizationParams: {
+          ...options.authorizationParams,
+          organization: orgID,
+        },
+      });
+      setSelectedOrganization(orgID);
+    },
+    [setSelectedOrganization]
+  );
+
   return (
     <OrganizationsContext.Provider
-      value={{ organizations, selectedOrganization, switchOrganization }}
+      value={{
+        organizations,
+        selectedOrganization,
+        switchOrganization,
+        _switchOrganization,
+      }}
     >
       {children}
     </OrganizationsContext.Provider>
