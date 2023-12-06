58bf3c88b7e9b590df0ad90b71612517aef9dc09
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2023-12-06 05:31:24 +0200

    chore: streamline Scaffold defaults

--- src/ui/TopBar/TopBar.tsx
+++ src/ui/TopBar/TopBar.tsx
@@ -1,10 +1,9 @@
-import { Global } from '@emotion/react';
-import { useBreakpoints, useTheme } from '@orfium/ictinus';
+import { useBreakpoints } from '@orfium/ictinus';
 import { ReactNode } from 'react';
 import Logo from '../../assets/orfium_logo.svg';
 import { _useTopBarUtilitySlot } from '../../hooks/useTopBarUtilitySlot';
 import UserMenu, { UserMenuProps } from './components/UserMenu';
-import { backGround, TopAppBarWrapper, UserDefinedSlot, UserSection } from './TopBar.styles';
+import { TopAppBarWrapper, UserDefinedSlot, UserSection } from './TopBar.styles';
 
 export type TopBarProps = {
   utilitySlot?: ReactNode;
@@ -12,13 +11,11 @@ export type TopBarProps = {
 
 export function TopBar({ utilitySlot, menuItems = [] }: TopBarProps) {
   const { topBarUtilitySlot } = _useTopBarUtilitySlot();
-  const theme = useTheme();
   const breakpoints = useBreakpoints();
   const isDesktop = breakpoints.des1366;
 
   return (
     <TopAppBarWrapper role="banner" aria-label="Top Application Banner">
-      <Global styles={{ body: backGround(theme) }} />
       {isDesktop ? null : <img alt={'Orfium logo'} src={Logo} height={28} width={28} />}
       <UserDefinedSlot>{topBarUtilitySlot || utilitySlot}</UserDefinedSlot>
       <UserSection>
