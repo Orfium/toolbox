9057a6b1384ea4b59de0bbd969554a793e3f5bfc
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2024-02-14 16:44:31 +0200

    chore: rename internal state and setter function
    Resolves comment https://app.graphite.dev/github/pr/Orfium/toolbox/126/feat-add-new-hook-useContentLoadingIndicator#comment-PRRC_kwDOHH8Ets5YuuRW

--- src/contexts/content-loading-indicator.ts
+++ src/contexts/content-loading-indicator.ts
@@ -1,13 +1,13 @@
 import { createContext, Dispatch, SetStateAction } from 'react';
 
 export type ContentLoadingIndicatorContextValue = {
-  loadingIndicatorActive: boolean;
-  setLoadingIndicatorActive: Dispatch<SetStateAction<boolean>>;
+  loadingIndicator: boolean;
+  setLoadingIndicator: Dispatch<SetStateAction<boolean>>;
 };
 
 export const defaultContentLoadingIndicatorContextValue: ContentLoadingIndicatorContextValue = {
-  loadingIndicatorActive: false,
-  setLoadingIndicatorActive: new Proxy(() => {}, {
+  loadingIndicator: false,
+  setLoadingIndicator: new Proxy(() => {}, {
     apply: () => {
       throw new Error(
         'You can only use useContentLoadingIndicator inside descendants of the Scaffold component'
