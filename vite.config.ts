cb9912a43002e4048a4b3b366377a481d0de4692
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2024-07-17 14:31:06 +0300

    fix: minor fix on vite.config for emotion on importSource

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { exec } from 'node:child_process';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, PluginOption } from 'vite';
 import dts from 'vite-plugin-dts';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -16,7 +16,7 @@ import pkg from './package.json';
 const regexesOfPackages = (externalPackages = []) =>
   externalPackages.map((packageName) => new RegExp(`^${packageName}(/.*)?`));
 
-const viteOnBuildSuccess = () => {
+const viteOnBuildSuccess = (): PluginOption => {
   return {
     name: 'vite:on-build-success',
     apply: 'build',
@@ -33,7 +33,6 @@ const plugins = [
   react({
     babel: {},
     jsxImportSource: '@emotion/react',
-    jsxRuntime: 'classic',
   }),
   tsconfigPaths(),
   svgr(),
@@ -52,7 +51,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    publicDir: false,
+    // publicDir: false,
     envPrefix: 'REACT_APP_',
     // Define these to keep compatibility with ictinus, toolbox and SSO
     define: {
@@ -67,7 +66,7 @@ export default defineConfig(({ mode }) => {
           ? {
               include: 'src/**',
             }
-          : false,
+          : null,
       lib: {
         entry: path.resolve(__dirname, 'src/index.ts'),
         name: pkg.name,
@@ -79,6 +78,7 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         external: [
           'react',
+          'react/jsx-runtime',
           'react-dom',
           /@emotion\/styled/,
           /@emotion\/react/,
